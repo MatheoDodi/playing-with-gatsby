24f41d467b48cb91c1fffb3f7f324ffeaea39c50
MatthewDodi <matthew.dodi@gmail.com>
2019-01-21 23:12:35 -0800

    added gatsby source filesystem and gatsby transformer remark

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,7 +26,15 @@ module.exports = {
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
       }
-    }
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/src/posts`,
+        name: 'posts'
+      }
+    },
+    'gatsby-transformer-remark'
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
